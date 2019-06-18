b72981211db815b762bc5044c3a3452d9f14ae62
bessserh <bessserh@gmail.com>
2019-06-19 00:31:09 +0300

    My new, first commit

--- ProjectDONE/settings.py
+++ ProjectDONE/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'bfa*9&^ybgy!siem6c@w02o!+2p8d*h29++alrs4v)vv1af&(n'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'bfa*9&^ybgy!siem6c@w02o!+2p8d*h29++alrs4v)vv1af&(n')
+SECRET_KEY = 'bfa*9&^ybgy!siem6c@w02o!+2p8d*h29++alrs4v)vv1af&(n'
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'bfa*9&^ybgy!siem6c@w02o!+2p8d*h29++alrs4v)vv1af&(n')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = True
+#DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -124,26 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
